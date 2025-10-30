cba26c08e5d7144de3a3d0c74db0208fc95ca7da
Dana-f559 <bogdanafarace559@gmail.com>
2025-10-30 17:28:26 +0200

    fixed minor things

--- drop.c
+++ drop.c
@@ -1,8 +1,8 @@
 #include <raylib.h>
+#include <raymath.h>
 #include <stddef.h>
 #include <stdlib.h>
 #include <math.h>
-#include <raymath.h>
 
 #include "drop.h"
 
@@ -12,17 +12,7 @@ typedef struct {
   Color color;
 } Drop;
 
-/**
- * Create a heap allocated circular drop with vcount number of vertices around
- * center.
- *
- * @param center The center of the circle.
- * @param radius The radius of the circle.
- * @param vcount The number of vertices.
- * @param color Color of the drop.
- *
- * @return Heap allocated pointer of the created Drop.
- */
+
 Drop circularDrop(Vector2 center, float radius, size_t vcount, Color color) {
     Drop drop;
     drop.vcount = vcount;
@@ -51,11 +41,6 @@ Drop circularDrop(Vector2 center, float radius, size_t vcount, Color color) {
     return drop;
 }
 
-/**
- * Free any resources allocated by the drop.
- *
- * @param drop The input drop structure to be destroyed.
- */
 void destroyDrop(Drop drop) {
     if (drop.vertices != NULL) {
         free(drop.vertices);
@@ -66,11 +51,4 @@ void drawDrop(Drop drop) {
     DrawTriangleFan(drop.vertices, drop.vcount, drop.color);
 }
 
-/**
- * Marble the input drop given a center. Using this equation:
- *   p = c + (p - c) * sqrt(1 + r^2 / (p - c)^2)
- * where p is a vertex point.
- *
- * @param drop_ptr Pointer of the drop to be marbled.
- */
-void marbleDrop(Drop *drop_ptr, Vector2 center);
+void marbleDrop(Drop *drop_ptr, Vector2 center) { /* TODO */};
--- drop.h
+++ drop.h
@@ -7,13 +7,38 @@ typedef struct {
     Color color;        // color of the drop
 } Drop;
 
+/**
+ * Create a heap allocated circular drop with vcount number of vertices around
+ * center.
+ *
+ * @param center The center of the circle.
+ * @param radius The radius of the circle.
+ * @param vcount The number of vertices.
+ * @param color Color of the drop.
+ *
+ * @return Heap allocated pointer of the created Drop.
+ */
 Drop circularDrop(Vector2 center, float radius, size_t vcount, Color color);
 
-// Free memory allocated by a drop
+/**
+ * Free any resources allocated by the drop.
+ *
+ * @param drop The input drop structure to be destroyed.
+ */
 void destroyDrop(Drop *drop);
 
-// Draw the drop using Raylib (probably DrawTriangleFan)
+/**
+  * Draw the drop.
+  *
+  * @param drop The input drop to be drawen.
+ */
 void drawDrop(Drop drop);
 
-// Distort ("marble") the drop using the given formula
+/**
+ * Marble the input drop given a center. Using this equation:
+ *   p = c + (p - c) * sqrt(1 + r^2 / (p - c)^2)
+ * where p is a vertex point.
+ *
+ * @param drop_ptr Pointer of the drop to be marbled.
+ */
 void marbleDrop(Drop *drop, Vector2 center);
